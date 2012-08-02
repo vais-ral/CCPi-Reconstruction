30deab043ffce927c337022ae3ea1ad7629100c8
bgs <bgs@841d8f18-27cf-4871-96e7-c32ca9c5df53>
2012-08-02 11:49:56 +0000

    Initial changes for portability to Microsoft Visual Studio

--- trunk/c/CBbackproject_single_newgeom_c.c
+++ trunk/c/CBbackproject_single_newgeom_c.c
@@ -40,20 +40,6 @@ static inline double max_dbl(double a, double b)
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	/* check number of arguments */
-	if(nrhs != 11)
-    {
-		mexWarnMsgTxt("11 inputs required. Aborting");
-		return;
-    }
-	else if(nlhs < 1)
-    {
-		mexWarnMsgTxt("At least one output required. Aborting");
-		return;
-	}
-
-
-
 	int i, curr_angle, im_size, curr_ray_y, curr_ray_z, n_angles;
 	long n_rays_y, n_rays_z, ray_offset;
 	mwSize im_size_matlab[3];
@@ -66,6 +52,18 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
 	struct jacobs_options options;
 	
+	/* check number of arguments */
+	if(nrhs != 11)
+    {
+		mexWarnMsgTxt("11 inputs required. Aborting");
+		return;
+    }
+	else if(nlhs < 1)
+    {
+		mexWarnMsgTxt("At least one output required. Aborting");
+		return;
+	}
+
 	/* read variables */
 	size_doubles = mxGetPr(prhs[0]);
 	for(i = 0; i < 3; i++)
@@ -102,73 +100,76 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     plhs[0] = mxCreateNumericMatrix(im_size_matlab[0]*im_size_matlab[1]*im_size_matlab[2], 1, mxSINGLE_CLASS, mxREAL);
     vol_data = (float *) mxGetData(plhs[0]);
 
-#pragma omp parallel shared(source_x, source_y, source_z, det_x, det_y, det_z, im_size, vol_data, angles, im_size_matlab, grid_offset, voxel_size) private(curr_angle, curr_ray_y, curr_ray_z, cos_curr_angle, sin_curr_angle, start, end, ray_offset, options, threadid, nthreads), firstprivate(n_rays_y, n_rays_z, n_angles, ray_data)
+#pragma omp parallel shared(source_x, source_y, source_z, det_x, det_y, det_z, im_size, vol_data, angles, im_size_matlab, grid_offset, voxel_size) private(curr_angle, curr_ray_y, curr_ray_z, cos_curr_angle, sin_curr_angle, start, end, ray_offset, options), firstprivate(n_rays_y, n_rays_z, n_angles, ray_data)
   {
+    int nz_offset, nz_step;
+    int nthreads = omp_get_num_threads();
+    int threadid = omp_get_thread_num();
 
-    nthreads = omp_get_num_threads();
-    threadid = omp_get_thread_num();
-
-  int nzblocks = nthreads;
-  int nz_size = im_size_matlab[2] / nzblocks;
-  if (nz_size * nzblocks < im_size_matlab[2])
-    nz_size++;
-  int nz_offset = threadid * nz_size;
-  int nz_step = nz_size;
+    int nzblocks = nthreads;
+    int nz_size = im_size_matlab[2] / nzblocks;
+    if (nz_size * nzblocks < im_size_matlab[2])
+      nz_size++;
+    nz_offset = threadid * nz_size;
+    nz_step = nz_size;
     if (nz_offset + nz_step > im_size_matlab[2])
       nz_step = im_size_matlab[2] - nz_offset;
-    printf("  z %d %d\n", nz_offset, nz_step);
+    if (nz_step > 0) {
+      /*printf("  z %d %d\n", nz_offset, nz_step);*/
 
-    options.im_size_default = 0;
-    options.im_size_x = im_size_matlab[0];
-    options.im_size_y = im_size_matlab[1];
-    options.im_size_z = nz_step;
+      options.im_size_default = 0;
+      options.im_size_x = im_size_matlab[0];
+      options.im_size_y = im_size_matlab[1];
+      options.im_size_z = nz_step;
 
-    options.b_default = 0;
-    options.b_x = grid_offset[0];
-    options.b_y = grid_offset[1];
-    options.b_z = grid_offset[2] + voxel_size[2] * nz_offset;
+      options.b_default = 0;
+      options.b_x = grid_offset[0];
+      options.b_y = grid_offset[1];
+      options.b_z = grid_offset[2] + voxel_size[2] * nz_offset;
 
-    options.d_x = voxel_size[0];
-    options.d_y = voxel_size[1];
-    options.d_z = voxel_size[2];
+      options.d_x = voxel_size[0];
+      options.d_y = voxel_size[1];
+      options.d_z = voxel_size[2];
 
-    for(curr_ray_z = 0; curr_ray_z < n_rays_z; curr_ray_z++) {
-      start[2] = *source_z;
-      end[2] = det_z[curr_ray_z];
+      for(curr_ray_z = 0; curr_ray_z < n_rays_z; curr_ray_z++) {
+	double delta_z, alpha_z_0, alpha_z_N;
+	double alpha_z_min, alpha_z_max, alpha_min, alpha_max;
+	start[2] = *source_z;
+	end[2] = det_z[curr_ray_z];
 
-      double delta_z = end[2] - start[2];
-      double alpha_z_0 = (r_i(0, options.b_z, voxel_size[2]) - start[2]) / delta_z;
-      double alpha_z_N = (r_i(nz_step, options.b_z, voxel_size[2]) - start[2]) / delta_z;
-      double alpha_z_min = min_dbl(alpha_z_0, alpha_z_N);
-      double alpha_z_max = max_dbl(alpha_z_0, alpha_z_N);
-      double alpha_min = max_dbl(0.0, alpha_z_min);
-      double alpha_max = min_dbl(1.0, alpha_z_max);
+	delta_z = end[2] - start[2];
+	alpha_z_0 = (r_i(0, options.b_z, voxel_size[2]) - start[2]) / delta_z;
+	alpha_z_N = (r_i(nz_step, options.b_z, voxel_size[2]) - start[2]) / delta_z;
+	alpha_z_min = min_dbl(alpha_z_0, alpha_z_N);
+	alpha_z_max = max_dbl(alpha_z_0, alpha_z_N);
+	alpha_min = max_dbl(0.0, alpha_z_min);
+	alpha_max = min_dbl(1.0, alpha_z_max);
 
-      if (alpha_min < alpha_max) {
-	for(curr_angle = 0; curr_angle < n_angles; curr_angle++) {
-	  /* rotate source and detector positions by current angle */
-	  cos_curr_angle = cos(angles[curr_angle]);
-	  sin_curr_angle = sin(angles[curr_angle]);
+	if (alpha_min < alpha_max) {
+	  for(curr_angle = 0; curr_angle < n_angles; curr_angle++) {
+	    /* rotate source and detector positions by current angle */
+	    cos_curr_angle = cos(angles[curr_angle]);
+	    sin_curr_angle = sin(angles[curr_angle]);
         
-	  start[0] = cos_curr_angle * (*source_x) - sin_curr_angle * (*source_y);
-	  start[1] = sin_curr_angle * (*source_x) + cos_curr_angle * (*source_y);
+	    start[0] = cos_curr_angle * (*source_x) - sin_curr_angle * (*source_y);
+	    start[1] = sin_curr_angle * (*source_x) + cos_curr_angle * (*source_y);
         
-	  ray_offset = curr_angle * n_rays_y * n_rays_z + curr_ray_z*n_rays_y;
+	    ray_offset = curr_angle * n_rays_y * n_rays_z + curr_ray_z*n_rays_y;
         
-	  /* loop over y values on detector */
-	  for(curr_ray_y = 0; curr_ray_y < n_rays_y; curr_ray_y++) {
-	    end[0] = cos_curr_angle * (*det_x) - sin_curr_angle * det_y[curr_ray_y];
-	    end[1] = sin_curr_angle * (*det_x) + cos_curr_angle * det_y[curr_ray_y];
+	    /* loop over y values on detector */
+	    for(curr_ray_y = 0; curr_ray_y < n_rays_y; curr_ray_y++) {
+	      end[0] = cos_curr_angle * (*det_x) - sin_curr_angle * det_y[curr_ray_y];
+	      end[1] = sin_curr_angle * (*det_x) + cos_curr_angle * det_y[curr_ray_y];
             
-	    /* loop over z values on detector */
-	    backproject_singledata(start, end,
-				   (double)ray_data[ray_offset + curr_ray_y],
-				   vol_data, &options, nz_offset);
+	      /* loop over z values on detector */
+	      backproject_singledata(start, end,
+				     (double)ray_data[ray_offset + curr_ray_y],
+				     vol_data, &options, nz_offset);
+	    }
 	  }
 	}
       }
     }
-    //nz_offset += nz_step;
   }    
 }
 
