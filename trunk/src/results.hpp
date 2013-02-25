cb6a79373fede746b802f7bdf50e210d5f7247b3
bgs <bgs@841d8f18-27cf-4871-96e7-c32ca9c5df53>
2013-02-25 15:39:09 +0000

    Tiff writing for output designed to give correct orientation in volview defaults.

--- trunk/src/results.hpp
+++ trunk/src/results.hpp
@@ -4,8 +4,8 @@
 
 namespace CCPi {
 
-  enum output_format { signed_byte_tiff, unsigned_byte_tiff, signed_short_tiff,
-		       unsigned_short_tiff, native_dump, bgs_float_dump };
+  enum output_format { unsigned_byte_tiff, unsigned_short_tiff,
+		       native_dump, signed_short_tiff, bgs_float_dump };
 
   void write_results(const std::string basename, const voxel_data &voxels,
 		     const real voxel_origin[3], const real voxel_size[3],
