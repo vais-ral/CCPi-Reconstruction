143a04afd523b98ffa490a7dd1fb3275b526813c
bgs <bgs@841d8f18-27cf-4871-96e7-c32ca9c5df53>
2013-05-03 10:53:44 +0000

    Cone beam find centre algorithm. Minor numerical differences from the Matlab version (0.245 vs 0.239 on bird skull). Plus fixes for XTek angle reading discovered in testing the algorithm.

--- src/instruments.hpp
+++ src/instruments.hpp
@@ -178,6 +178,7 @@ namespace CCPi {
     bool read_angles(const std::string datafile, const real init_angle,
 		     const int n);
     bool read_images(const std::string path);
+    void find_centre(const int v_slice);
   };
 
 }
