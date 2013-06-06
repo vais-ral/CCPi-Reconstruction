04f98a99361e25565ed2553d338113fea362641f
bgs <bgs@841d8f18-27cf-4871-96e7-c32ca9c5df53>
2013-06-06 13:21:56 +0000

    Changes to 2D slice matrix storage to permit use of sparse matrix-vector routines.

--- src/instruments.cpp
+++ src/instruments.cpp
@@ -195,11 +195,15 @@ void CCPi::parallel_beam::setup_projection_matrix(const real origin[3],
   timer ptime(USE_TIMER);
   // assumes 2D slices which makes the storage practical.
   setup_2D_matrix(get_h_pixels(), get_phi(), get_num_angles(),
-		  get_num_h_pixels(), origin, width, nx, ny, nz);
+		  get_num_v_pixels(), get_num_h_pixels(), origin, width,
+		  nx, ny, nz);
   has_projection_matrix = true;
   ptime.accumulate();
   ptime.output("projection map");
 }
+
+#define MKL_INT long
+#include "mkl_spblas.h"
 
 void CCPi::parallel_beam::forward_project_matrix(const real det_z[],
 						 pixel_type ray_data[],
@@ -221,24 +225,24 @@ void CCPi::parallel_beam::forward_project_matrix(const real det_z[],
     if (k < 0 or k >= nz_voxels)
       continue;
     long k_offset = k * nx_voxels * ny_voxels;
+    long z_offset = curr_ray_z * n_rays_y;
 
-    int offset = 0;
-    for (long curr_angle = 0; curr_angle < n_angles; curr_angle++) {
-
-      long ray_offset = curr_angle * long(n_rays_y) * long(n_rays_z)
-	+ curr_ray_z * long(n_rays_y);
-
-      /* loop over y values on detector */
-      for (long curr_ray_y = 0; curr_ray_y < n_rays_y; curr_ray_y++) {
-
-	real data = 0.0;
-	for (int i = 0; i < forward_sizes[offset]; i++)
-	  data += vol_data[k_offset + forward_y[offset][i] * nx_voxels
-			   + forward_x[offset][i]] * forward_data[offset][i];
-	ray_data[ray_offset + curr_ray_y] = data;
-	offset++;
-      }
-    }
+    for (long i = 0; i < matrix_size; i++)
+      ray_data[forward_rows[i] + z_offset] += forward_matrix[i]
+	* vol_data[k_offset + forward_cols[i]];
+    /*
+    char desc[6];
+    desc[0] = 'G';
+    desc[3] = 'C';
+    long n_rays = long(n_angles) * long(n_rays_z) * long(n_rays_y);
+    long n_vox = long(nx_voxels) * long(ny_voxels) * long(nz_voxels);
+    real alpha = 1.0;
+    real beta = 1.0;
+    long sz = matrix_size;
+    mkl_dcoomv("N", &n_rays, &n_vox, &alpha, desc, forward_matrix,
+	       forward_rows, forward_cols, &sz, &vol_data[k_offset],
+	       &beta, &ray_data[z_offset]);
+    */
   }
 }
 
@@ -256,37 +260,10 @@ void CCPi::parallel_beam::backward_project_matrix(const real det_z[],
 {
 #pragma omp parallel for shared(det_z, ray_data) schedule(dynamic)
   for (long k = 0; k < nz_voxels; k++) {
-    /*
-    // Todo - improve by calculating curr_ray_z range rather than 2 loops
-    // we loop on k to avoid possible any possible shared access to voxels
-    // Todo - if we stick with 2 loops is there a better loop order for k/z/j/i?
-    // just calc k range here then loop over it inside loop over p?
-    long k_offset = k * nx_voxels * ny_voxels;
-    for (long curr_ray_z = 0; curr_ray_z < n_rays_z; curr_ray_z++) {
-      long kz = (long)std::floor((det_z[curr_ray_z]
-				  - grid_offset[2]) / voxel_size[2]);
-      if (kz != k)
-	continue;
-
-      int offset = 0;
-      for (long j = 0; j < ny_voxels; j++) {
-	long j_offset = k_offset + j * nx_voxels;
-	for (long i = 0; i < nx_voxels; i++) {
-	  real data = 0.0;
-	  for (int p = 0; p < backward_sizes[offset]; p++)
-	    data += ray_data[backward_angles[offset][p] * long(n_rays_z)
-			     * long(n_rays_y) + curr_ray_z * long(n_rays_y)
-			     + backward_h[offset][p]]
-	      * backward_data[offset][p];
-	  vol_data[j_offset + i] = data;
-	  offset++;
-	}
-      }
-    }
-    */
     long k_offset = k * nx_voxels * ny_voxels;
     long z_min = -1;
     long z_max = -1;
+    // Todo - calculate range rather than looping
     for (long curr_ray_z = 0; curr_ray_z < n_rays_z; curr_ray_z++) {
       long kz = (long)std::floor((det_z[curr_ray_z]
 				  - grid_offset[2]) / voxel_size[2]);
@@ -298,22 +275,26 @@ void CCPi::parallel_beam::backward_project_matrix(const real det_z[],
 	break;
     }
 
-    int offset = 0;
-    for (long j = 0; j < ny_voxels; j++) {
-      long j_offset = k_offset + j * nx_voxels;
-      for (long i = 0; i < nx_voxels; i++) {
-	real data = 0.0;
-	for (int p = 0; p < backward_sizes[offset]; p++) {
-	  long step = backward_angles[offset][p] * long(n_rays_z)
-	    * long(n_rays_y) + z_min * long(n_rays_y) + backward_h[offset][p];
-	  for (long curr_ray_z = z_min; curr_ray_z <= z_max; curr_ray_z++) {
-	    data += ray_data[step] * backward_data[offset][p];
-	    step += long(n_rays_y);
-	  }
+    long size = nx_voxels * ny_voxels;
+    for (long curr_ray_z = z_min; curr_ray_z <= z_max; curr_ray_z++) {
+      long z_offset = curr_ray_z * n_rays_y;
+      for (long i = 0; i < size; i++) {
+	for (long j = backward_rowb[i]; j < backward_rowe[i]; j++) {
+	  vol_data[k_offset + i] += backward_matrix[j] *
+	    ray_data[backward_cols[j] + z_offset];
 	}
-	vol_data[j_offset + i] = data;
-	offset++;
       }
+      /*
+      char desc[6];
+      desc[0] = 'G';
+      desc[3] = 'C';
+      long n_rays = long(n_angles) * long(n_rays_z) * long(n_rays_y);
+      real alpha = 1.0;
+      real beta = 1.0;
+      mkl_dcsrmv("N", &size, &n_rays, &alpha, desc, backward_matrix,
+		 backward_cols, backward_rowb, backward_rowe,
+		 &ray_data[z_offset], &beta, &vol_data[k_offset]);
+      */
     }
   }
 }
