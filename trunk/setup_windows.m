2dbb66e271ada31a5bbe2fb89932eed19e906233
bgs <bgs@841d8f18-27cf-4871-96e7-c32ca9c5df53>
2014-03-31 10:19:24 +0000

    Create parallel beam example from cone beam phantom with mex projection files

--- trunk/setup_windows.m
+++ trunk/setup_windows.m
@@ -23,4 +23,6 @@
 
 mex mex/CBbackproject_single_newgeom_c.cpp -Isrc -Imex -outdir mex -largeArrayDims COMPFLAGS="$COMPFLAGS /Za /openmp /DWINDOWS"
 mex mex/CBproject_single_newgeom_c.cpp -Isrc -Imex -outdir mex -largeArrayDims COMPFLAGS="$COMPFLAGS /Za /openmp /DWINDOWS"
+mex mex/PBbackproject_single_newgeom_c.cpp -Isrc -Imex -outdir mex -largeArrayDims COMPFLAGS="$COMPFLAGS /Za /openmp /DWINDOWS"
+mex mex/PBproject_single_newgeom_c.cpp -Isrc -Imex -outdir mex -largeArrayDims COMPFLAGS="$COMPFLAGS /Za /openmp /DWINDOWS"
 mex mex/tvreg_upn_c.cpp src/tv_core.cpp src/instruments.cpp src/matrix2D.cpp src/timer.cpp -Isrc -Imex -outdir mex -largeArrayDims COMPFLAGS="$COMPFLAGS /Za /openmp /DWINDOWS"
