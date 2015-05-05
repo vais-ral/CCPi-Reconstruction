1cf8a82d644c73d9f48114cca226c2c571913973
bgs <bgs@841d8f18-27cf-4871-96e7-c32ca9c5df53>
2015-05-05 13:27:10 +0000

    Import landweberLS from Daniil's interative project (IterLS/lansweberLS.m)

---
+++ src/landweber.hpp
@@ -0,0 +1,34 @@
+
+#ifndef CCPI_RECON_LANDWEBER
+#define CCPI_RECON_LANDWEBER
+
+namespace CCPi {
+
+  class landweberLS : public reconstruction_alg {
+  public:
+    landweberLS(const int niterations, const real lam);
+
+    bool reconstruct(class instrument *device, voxel_data &voxels,
+		     const real origin[3], const real voxel_size[3]);
+
+  protected:
+    int get_iterations() const;
+
+  private:
+    int iterations;
+    real lambda;
+  };
+
+}
+
+inline CCPi::landweberLS::landweberLS(const int niterations, const real lam)
+  : iterations(niterations), lambda(lam)
+{
+}
+
+inline int CCPi::landweberLS::get_iterations() const
+{
+  return iterations;
+}
+
+#endif // CCPI_RECON_LANDWEBER
