7f88908dfce7a0b1884d231f62a31305110ae28e
bgs <bgs@841d8f18-27cf-4871-96e7-c32ca9c5df53>
2015-05-18 14:32:25 +0000

    Update cone bream reconstruction module

--- trunk/Avizo/src/CCPiReconstruction/Cone_Beam_recon.cpp
+++ trunk/Avizo/src/CCPiReconstruction/Cone_Beam_recon.cpp
@@ -24,9 +24,21 @@ HX_INIT_CLASS(Cone_Beam_recon,HxCompModule)
 
 Cone_Beam_recon::Cone_Beam_recon() :
     HxCompModule(HxUniformScalarField3::getClassTypeId()),
-  portAction(this,"action",QApplication::translate("Cone_Beam_recon", "Action")),
-  rotationAngle(this,"rotation angle",QApplication::translate("Cone_Beam_recon", "Rotation Angle")),
-  pixelSize(this,"pixel size",QApplication::translate("Cone_Beam_recon", "Pixel Size(x,y)")),
+  portAction(this,"action",QApplication::translate("Cone_Beam_recon",
+						   "Action")),
+  rotationAngle(this,"rotation angle",QApplication::translate("Cone_Beam_recon",
+							    "Rotation Angle")),
+// Todo
+//horizontalShifts(this,"horizontal shifts",
+//	   QApplication::translate("Cone_Beam_recon",
+//				   "Horizontal Shifts")),
+//verticalShifts(this,"vertical shifts",
+//	 QApplication::translate("Cone_Beam_recon", "Vertical Shifts")),
+  pixelSize(this,"pixel size",QApplication::translate("Cone_Beam_recon",
+						      "Pixel Size(x,y)")),
+  coneGeometry(this,"geometry",
+	       QApplication::translate("Cone_Beam_recon",
+				       "Cone Geometry(SrcObj/SrcDetector)")),
   algorithm(this, "algorithm",
 	    QApplication::translate("Cone_Beam_recon",
 				    "Reconstruction Algorithm")),
@@ -38,7 +50,11 @@ Cone_Beam_recon::Cone_Beam_recon() :
 	      QApplication::translate("Cone_Beam_recon", "Beam Hardening"))
 {
   rotationAngle.addType(HxUniformScalarField3::getClassTypeId());
+  // Todo - these should be optional
+  //horizontalShifts.addType(HxUniformScalarField3::getClassTypeId());
+  //verticalShifts.addType(HxUniformScalarField3::getClassTypeId());
   pixelSize.addType(HxUniformScalarField3::getClassTypeId());
+  coneGeometry.addType(HxUniformScalarField3::getClassTypeId());
 
   portAction.setLabel(0,"DoIt");
   algorithm.setNum(2);
@@ -66,15 +82,21 @@ void Cone_Beam_recon::compute()
     // Check whether the input port is connected
     if (field == 0)
       return;
-    HxUniformScalarField3* rot_angle =
-      (HxUniformScalarField3*) rotationAngle.source();
+    HxUniformScalarField3 *rot_angle =
+      (HxUniformScalarField3 *) rotationAngle.source();
     //Check the Rotation Angle
     if (rot_angle == 0 or rot_angle->lattice.dims()[0] == 1)
       return;
+    // Todo - check of optional shifts
     //Check the Pixels
-    HxUniformScalarField3* pixel_size =
-      (HxUniformScalarField3*) pixelSize.source();
+    HxUniformScalarField3 *pixel_size =
+      (HxUniformScalarField3 *) pixelSize.source();
     if (pixel_size == 0 or pixel_size->lattice.dims()[0] != 2)
+      return;
+    //Check the Geometry
+    HxUniformScalarField3 *cone_geom =
+      (HxUniformScalarField3 *) coneGeometry.source();
+    if (cone_geom == 0 or cone_geom->lattice.dims()[0] != 2)
       return;
 
     // Todo - check that its a float field?
@@ -89,29 +111,41 @@ void Cone_Beam_recon::compute()
 
 void Cone_Beam_recon::run_reconstruction()
 {
-  HxUniformScalarField3* field = (HxUniformScalarField3*) portData.source();
+  HxUniformScalarField3 *field = (HxUniformScalarField3 *) portData.source();
   const int *fdims = field->lattice.dims();
   boost::multi_array_ref<float, 3>
     pixels((float *)field->lattice.dataPtr(),
 	   boost::extents[fdims[0]][fdims[1]][fdims[2]],
 	   boost::fortran_storage_order());
-  HxUniformScalarField3* rot_angle =
-    (HxUniformScalarField3*) rotationAngle.source();
+  HxUniformScalarField3 *rot_angle =
+    (HxUniformScalarField3 *) rotationAngle.source();
   boost::multi_array_ref<float, 1> angles((float *)rot_angle->lattice.dataPtr(),
 					  boost::extents[fdims[2]]);
 
-  // Todo - offsets and geometry data
+  // Todo - offsets
   boost::multi_array<float, 1> h_offsets(boost::extents[1]);
   boost::multi_array<float, 1> v_offsets(boost::extents[1]);
-  real source_x = -1.0;
-  real detector_x = 10.0;
-  real h_size = 0.1;
-  real v_size = 0.1;
-  // end Todo
+
+  HxUniformScalarField3 *pix = (HxUniformScalarField3* ) pixelSize.source();
+  float *pixel_ptr = (float *)pix->lattice.dataPtr();
+  real h_size = pixel_ptr[0];
+  real v_size = pixel_ptr[1];
+  HxUniformScalarField3 *geom = (HxUniformScalarField3 *) coneGeometry.source();
+  float *geom_ptr = (float *)geom->lattice.dataPtr();
+  real source_x = - geom_ptr[0];
+  real detector_x = geom_ptr[1] + source_x;
+
   int pixels_per_voxel = resolution.getValue();
   int niterations = iterations.getValue();
   bool beam_hardening = beam_harden.getValue();
-  CCPi::instrument *instrument = new CCPi::Nikon_XTek();
+
+  CCPi::instrument *instrument = 0;
+  // Todo - need a type that supports this and will probably not be able
+  // to use the find_centre algorithm with it.
+  // if (optional shifts set)
+  // instrument = ?;
+  // else
+  instrument = new CCPi::Nikon_XTek();
 
   CCPi::reconstruction_alg *recon_algorithm = 0;
   switch (algorithm.getValue()) {
@@ -127,10 +161,12 @@ void Cone_Beam_recon::run_reconstruction()
 
   machine::initialise(0);
   // instrument setup from pixels/angles will probably copy
+  real vox_origin[3];
+  real vox_size[3];
   voxel_data *voxels = reconstruct(instrument, recon_algorithm, pixels, angles,
 				   h_offsets, v_offsets, pixels_per_voxel,
 				   source_x, detector_x, h_size, v_size,
-				   beam_hardening);
+				   beam_hardening, vox_origin, vox_size);
   machine::exit();
   delete recon_algorithm;
   delete instrument;
@@ -147,7 +183,15 @@ void Cone_Beam_recon::run_reconstruction()
 	for (int k = 0; k < dims[2]; k++)
 	  output->set(i, j, k, (*voxels)[i][j][k]);
     delete voxels;
-    // Todo - some sort of setBoundingBox for output? based on origin etc
+    HxUniformCoord3 *coords =(HxUniformCoord3 *) output->lattice.coords();
+    float *bx = coords->bbox();
+    bx[0] = vox_origin[2];
+    bx[1] = vox_origin[2] + float(dims[0]) * vox_size[2];
+    bx[2] = vox_origin[1];
+    bx[3] = vox_origin[1] + float(dims[0]) * vox_size[1];
+    bx[4] = vox_origin[0];
+    bx[5] = vox_origin[0] + float(dims[2]) * vox_size[0];
+    // publish reconstruction
     setResult(output); 
   }
 }
