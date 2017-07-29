0b6c87ff3ea8bec357c7830696d8657a216cdf21
Srikanth Nagella <srikanthnag@gmail.com>
2017-07-29 21:46:29 +0100

    Update bld.bat

--- python/conda-recipe/bld.bat
+++ python/conda-recipe/bld.bat
@@ -1,3 +1,10 @@
+SET CIL_VERSION 2>Nul | Findstr/I "."
+IF ERRORLEVEL 1  
+(
+ECHO CIL_VERSION Not Defined.
+exit 1
+)
+
 mkdir "%SRC_DIR%\ccpi"
 xcopy /e "%RECIPE_DIR%\..\.." "%SRC_DIR%\ccpi"
 cd ccpi\python
