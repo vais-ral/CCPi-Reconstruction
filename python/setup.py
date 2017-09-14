16bc3812c10066dd2e239a4bd09528c2ab9065a8
Edoardo Pasca <edo.paskino@gmail.com>
2017-09-14 16:24:50 +0100

    Added numpy.get_include() for linux

--- python/setup.py
+++ python/setup.py
@@ -8,16 +8,31 @@ from Cython.Distutils import build_ext
 import os
 import numpy
 import platform	
+import sys
+
 cil_version=os.environ['CIL_VERSION']
 if  cil_version == '':
     print("Please set the environmental variable CIL_VERSION")
     sys.exit(1)
 
+try:
+    library_include_path = os.environ['LIBRARY_INC']
+except:
+    if platform.system() == 'Windows':
+        pass
+    else:
+        try:
+           library_include_path = os.environ['PREFIX']+'/include'
+        except:
+           pass
+    pass
 extra_compile_args = ['-fopenmp','-O2', '-funsigned-char', '-Wall', '-Werror']
 extra_libraries = []
+extra_include_dirs = []
+extra_library_dirs = []
 if platform.system() == 'Windows':
    extra_compile_args[0:] = ['/DWIN32','/EHsc','/DBOOST_ALL_NO_LIB' , '/openmp']   
-   extra_include_dirs += ["..\\src\\","..\\src\\Algorithms","..\\src\\Readers", "."]
+   extra_include_dirs += ["..\\src\\","..\\src\\Algorithms","..\\src\\Readers", ".", numpy.get_include()]
    extra_include_dirs += [library_include_path]
    extra_library_dirs += [r'C:\Apps\Miniconda2\envs\cil\Library\lib']
    if sys.version_info.major == 3 :   
@@ -26,7 +41,7 @@ if platform.system() == 'Windows':
        extra_libraries += ['boost_python-vc140-mt-1_64', 'boost_numpy-vc140-mt-1_64']   
 else:
    extra_include_dirs += ['/apps/anaconda/2.4/envs/ccpi-py2/include/','/apps/anaconda/2.4/envs/ccpi-py2/include/python2.7']
-   extra_include_dirs += ["../src/","../src/Algorithms","../src/Readers", "."]
+   extra_include_dirs += ["../src/","../src/Algorithms","../src/Readers", ".", numpy.get_include()]
    extra_include_dirs += [library_include_path]
    if sys.version_info.major == 3 :
        extra_libraries += ['boost_python3', 'boost_numpy3','gomp']
