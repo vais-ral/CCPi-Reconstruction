cbf2718f3a4b5b617fb8afcc0b70f44410936495
Edoardo Pasca <edo.paskino@gmail.com>
2017-12-01 08:15:56 +0000

    fixing iteration control

--- Wrappers/python/src/diamond_wrapper.cpp
+++ Wrappers/python/src/diamond_wrapper.cpp
@@ -137,12 +137,100 @@ np::ndarray reconstruct_iter(np::ndarray ndarray_pixels,
 }
 
 /*********WITH PREVIOUS STEP************/
-np::ndarray reconstruct_iter(np::ndarray ndarray_pixels,
+bp::list initialize_cgls(np::ndarray ndarray_pixels,
 	np::ndarray ndarray_angles,
 	double rotation_centre, int resolution,
 	int niterations, int nthreads,
 	CCPi::algorithms alg,
-	np::ndarray last_iteration_volume ,
+	const real regularize = 0.0,
+	np::ndarray norm = np::zeros(bp::make_tuple(0), np::dtype::get_builtin<float>()), bool is_pixels_in_log = true)
+{
+	bool beam_harden = false;
+	// vertical size to break data up into for processing
+	const int blocking_factor = 0;
+	// number of GPUs etc if using accelerated code
+	//const int num_devices = 1;
+	numpy_3d pixels(reinterpret_cast<float*>(ndarray_pixels.get_data()), boost::extents[ndarray_pixels.shape(0)][ndarray_pixels.shape(1)][ndarray_pixels.shape(2)]);
+	numpy_1d angles(reinterpret_cast<float*>(ndarray_angles.get_data()), boost::extents[ndarray_angles.shape(0)]);
+	voxel_data *voxels = 0;
+	CCPi::reconstruction_alg *algorithm = 0;
+	switch (alg) {
+	case CCPi::alg_CGLS:
+		//if (blocking_factor > 0 and instrument->supports_blocks())
+		//  recon_algorithm = new CCPi::cgls_2d(niterations, pixels_per_voxel);
+		algorithm = new CCPi::cgls_3d(niterations);
+		break;
+	case CCPi::alg_SIRT:
+		algorithm = new CCPi::sirt(niterations);
+		break;
+	case CCPi::alg_MLEM:
+		algorithm = new CCPi::mlem(niterations);
+		break;
+	case CCPi::alg_CGLS_Tikhonov:
+		algorithm = new CCPi::cgls_tikhonov(niterations, regularize);
+		break;
+	case CCPi::alg_CGLS_TVreg:
+		algorithm = new CCPi::cgls_tv_reg(niterations, regularize);
+		break;
+	default:
+		break;
+	}
+	if (algorithm != 0) {
+		CCPi::instrument *instrument = new CCPi::Diamond();
+		machine::initialise(nthreads);
+		// instrument setup from pixels/angles will probably copy
+		voxels = reconstruct(instrument, algorithm, pixels, angles, rotation_centre,
+			resolution, blocking_factor, beam_harden, is_pixels_in_log);
+		machine::exit();
+		delete instrument;
+		if (norm[0] != 0) {
+			if (voxels == 0) {
+				for (int i = 0; i < niterations; i++)
+					norm[i] = 0.0;
+			}
+			else {
+				real_1d data(niterations);
+				for (int i = 0; i < niterations; i++)
+					norm[i] = data[i];
+			}
+		}
+		delete algorithm;
+	}
+	int dims[3];
+	if (voxels == 0) {
+		dims[0] = 1;
+		dims[1] = 1;
+		dims[2] = 1;
+	}
+	else {
+		// Todo - remove buffered region
+		dims[0] = voxels->shape()[0];
+		dims[1] = voxels->shape()[1];
+		dims[2] = voxels->shape()[2];
+	}
+
+	np::dtype dt = np::dtype::get_builtin<float>();
+	bp::tuple shape = bp::make_tuple(dims[0], dims[1], dims[2]);
+	np::ndarray varray = np::zeros(shape, dt);
+	if (voxels == 0)
+		varray[0][0][0] = 0.0;
+	else {
+		// Todo - vector? and remove buffered region
+		for (int i = 0; i < dims[0]; i++)
+			for (int j = 0; j < dims[1]; j++)
+				for (int k = 0; k < dims[2]; k++)
+					varray[i][j][k] = (*voxels)[i][j][k];
+		delete voxels;
+	}
+	return varray;
+}
+
+bp::list reconstruct_iter(np::ndarray ndarray_pixels,
+	np::ndarray ndarray_angles,
+	double rotation_centre, int resolution,
+	int niterations, int nthreads,
+	CCPi::algorithms alg,
+	bp::list last_iteration ,
 	const real regularize = 0.0,
 	np::ndarray norm = np::zeros(bp::make_tuple(0), np::dtype::get_builtin<float>()), bool is_pixels_in_log = true
 	)
@@ -160,6 +248,8 @@ np::ndarray reconstruct_iter(np::ndarray ndarray_pixels,
 	Copy the last_iteration_volume numpy array into the
 	voxels boost multi array
 	*/
+	np::ndarray last_iteration_volume = bp::extract<np::ndarray>(last_iteration[0]);
+
 	int nx_voxels = last_iteration_volume.shape(0);
 	int ny_voxels = last_iteration_volume.shape(1);
 	int nz_voxels = last_iteration_volume.shape(2);
--- Wrappers/python/src/diamond_wrapper.hpp
+++ Wrappers/python/src/diamond_wrapper.hpp
@@ -24,7 +24,13 @@ reconstruct_cgls_step(np::ndarray pixels,
 	np::ndarray angles,
 	double rotation_centre, int resolution,
 	int niterations, int nthreads, bool is_pixels_in_log,
-	np::ndarray last_iteration_volume);
+	bp::list last_iteration);
+
+extern bp::list initialize_cgls(np::ndarray pixels,
+	np::ndarray angles,
+	double rotation_centre, int resolution,
+	int niterations, int nthreads, bool is_pixels_in_log);
+
 
 extern np::ndarray
 reconstruct_sirt(np::ndarray pixels,
