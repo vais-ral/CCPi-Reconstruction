a1fa0c4fb150b6e1cddea70bfb6988a0f6a40183
Edoardo Pasca <edo.paskino@gmail.com>
2017-12-12 16:29:44 +0000

    fixes for conda build (linux)

--- recipes/library/build.sh
+++ recipes/library/build.sh
@@ -1,14 +1,16 @@
-if [ -z "$CIL_VERSION" ]; then
-    echo "Need to set CIL_VERSION"
-    exit 1
-fi  
-
+#if [ -z "$CIL_VERSION" ]; then
+#    echo "Need to set CIL_VERSION"
+#    exit 1
+#fi  
+export CIL_VERSION=0.9.1
 
 
 
 
 mkdir ${SRC_DIR}/build
-cp -r "${RECIPE_DIR}/../../Core" ${SRC_DIR}/build
-cd ${SRC_DIR}/build
+cp -rv ${RECIPE_DIR}/../../Core/ ${SRC_DIR}/build
+mkdir ${SRC_DIR}/build/build
+cd ${SRC_DIR}/build/build
+cmake -G "Unix Makefiles" ../Core
 
-cmake -G "Unix Makefiles" ${SRC_DIR}/build
+make -j2
