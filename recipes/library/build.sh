f85cad09dfe3a47fec202db3955eaa19e6c78635
Edoardo Pasca <edo.paskino@gmail.com>
2017-12-13 22:14:27 +0000

    Compiles and installs core reconstruction lib

--- recipes/library/build.sh
+++ recipes/library/build.sh
@@ -1,3 +1,4 @@
+echo $CIL_VERSION
 #if [ -z "$CIL_VERSION" ]; then
 #    echo "Need to set CIL_VERSION"
 #    exit 1
@@ -6,12 +7,11 @@ export CIL_VERSION=0.9.1
 
 
 
-
 mkdir ${SRC_DIR}/build
 cp -rv ${RECIPE_DIR}/../../Core/ ${SRC_DIR}/build
 mkdir ${SRC_DIR}/build/build
 cd ${SRC_DIR}/build/build
-cmake -G "Unix Makefiles" ../Core
+cmake -G "Unix Makefiles" -DLIBRARY_LIB="${CONDA_PREFIX}/lib" -DLIBRARY_INC="${CONDA_PREFIX}" -DCMAKE_INSTALL_PREFIX="${PREFIX}" ../Core
 
 make -j2 VERBOSE=1
 make install
